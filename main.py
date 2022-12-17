0f50b530c9fe27e35d4ab2eee5e99a63afa452a7
vwxwv1 <61165141+vwxwv1@users.noreply.github.com>
2022-12-16 20:54:22 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,52 @@
+from flask import Flask,jsonify,request
+import csv
+import pandas as pd
+
+df1 = pd.read_csv('./articles.csv')
+
+headerList = ['id', 'index', 'timestamp', 'eventType', 'contentId', 'authorPersonId', 'authorSessionId', 'authorUserAgent', 
+'authorRegion', 'authorCountry', 'contentType', 'url', 'title', 'text', 'lang', 'total_events']
+
+# Uneccesary because it is completed
+# df.to_csv("articles.csv", header=headerList, index=False)
+
+df1.to_csv("all_articles.csv", header=False, index=False)
+
+all_articles = []
+liked_articles = []
+not_liked_articles = []
+
+app = Flask(__name__)
+
+with open('all_articles.csv') as f:
+    reader = csv.reader(f)
+    data = list(reader)
+    all_articles = data[1:]
+
+@app.route("/get-article")
+def get_movie():
+    return jsonify({
+        "data" : all_articles[0],
+        "status" : "success"
+        })
+
+@app.route("/liked-articles",methods=["POST"])
+def liked_article():
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    liked_articles.append(article)
+    return jsonify({
+        "status" : "success"
+    }), 201
+
+@app.route("/unliked-not_liked_articles",methods=["POST"])
+def liked_movie():
+    article = all_articles[0]
+    all_articles = all_articles[1:]
+    not_liked_articles.append(article)
+    return jsonify({
+        "status" : "success"
+    }), 201
+
+if __name__ == "__main__":
+    app.run()
